dde540ef0218099026dc530b88dbbb8c7a405ef1
IanoNjuguna <ichege438@gmail.com>
2024-03-12 22:27:54 +0300

    game.py: set up the background image, input detection (key binding), and game Physics (collision detection)

--- game.py
+++ game.py
@@ -5,18 +5,47 @@ class Game:
 	def __init__(self) -> None:
 		pygame.init()
 
-
 		pygame.display.set_caption('yozaka')
-		self.screen = pygame.display.set_mode((640,480))
+		self.screen = pygame.display.set_mode((640, 480))
 
 		self.clock = pygame.time.Clock()
 
+		self.img = pygame.image.load('data/images/clouds/cloud_1.png')
+		self.img.set_colorkey((0, 0, 0))
+
+		self.img_pos = [160, 260]
+		self.movement = [False, False]
+
+		self.collision_area = pygame.Rect(50, 50, 300, 50)
+
 	def run(self):
 		while True:
+			self.screen.fill((114, 116, 248))
+
+			img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
+			if img_r.colliderect(self.collision_area):
+				pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
+			else:
+				pygame.draw.rect(self.screen, (0, 50, 155), self.collision_area)
+
+			self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5 
+			self.screen.blit(self.img, self.img_pos)
+
+
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					pygame.quit()
 					exit()
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_UP:
+						self.movement[0] = True
+					if event.key == pygame.K_DOWN:
+						self.movement[1] = True
+				if event.type == pygame.KEYUP:
+					if event.key == pygame.K_UP:
+						self.movement[0] = False
+					if event.key == pygame.K_DOWN:
+						self.movement[1] = False
 
 			pygame.display.update()
 			self.clock.tick(60)
