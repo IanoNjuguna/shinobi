8678967fdc4e1098e8884bf3b756c70f0483015f
IanoNjuguna <ichege438@gmail.com>
2024-03-12 23:29:33 +0300

    game.py: change key bindings (player's motion is right-left) && render the player onto the display
    scripts/entities.py: render the player's movement onto the screen on
    keypress
    scripts/utils.py: return the player's assets from the 'data/' dir

--- game.py
+++ game.py
@@ -1,5 +1,9 @@
-from sys import exit
 import pygame
+
+from sys import exit
+
+from game_scripts.utils import load_image
+from game_scripts.entities import PhysicsEntity
 
 class Game:
 	def __init__(self) -> None:
@@ -7,46 +11,41 @@ class Game:
 
 		pygame.display.set_caption('yozaka')
 		self.screen = pygame.display.set_mode((640, 480))
+		self.display = pygame.Surface((320, 240))
 
 		self.clock = pygame.time.Clock()
 
-		self.img = pygame.image.load('data/images/clouds/cloud_1.png')
-		self.img.set_colorkey((0, 0, 0))
-
-		self.img_pos = [160, 260]
 		self.movement = [False, False]
 
-		self.collision_area = pygame.Rect(50, 50, 300, 50)
+		self.assets = {
+			'player': load_image('entities/player.png')
+		}
+
+		self.player = PhysicsEntity(self, 'player', (50, 50), (8,15))
 
 	def run(self):
 		while True:
-			self.screen.fill((114, 116, 248))
+			self.display.fill((114, 116, 248))
 
-			img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
-			if img_r.colliderect(self.collision_area):
-				pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
-			else:
-				pygame.draw.rect(self.screen, (0, 50, 155), self.collision_area)
-
-			self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5 
-			self.screen.blit(self.img, self.img_pos)
-
+			self.player.update((self.movement[1] - self.movement[0], 0))
+			self.player.render(self.display)
 
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					pygame.quit()
 					exit()
 				if event.type == pygame.KEYDOWN:
-					if event.key == pygame.K_UP:
+					if event.key == pygame.K_LEFT or event.key == pygame.K_d:
 						self.movement[0] = True
-					if event.key == pygame.K_DOWN:
+					if event.key == pygame.K_RIGHT or event.key == pygame.K_a:
 						self.movement[1] = True
 				if event.type == pygame.KEYUP:
-					if event.key == pygame.K_UP:
+					if event.key == pygame.K_LEFT or event.key == pygame.K_d:
 						self.movement[0] = False
-					if event.key == pygame.K_DOWN:
+					if event.key == pygame.K_RIGHT or event.key == pygame.K_a:
 						self.movement[1] = False
 
+			self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
 			pygame.display.update()
 			self.clock.tick(60)
 
